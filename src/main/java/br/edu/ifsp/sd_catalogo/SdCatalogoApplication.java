7b5b5573edcad47a0c49e76d664259ab0b6a7a27
Luciano Araujo <luciano.ricardo@aluno.ifsp.edu.br>
2026-04-15 21:11:09 -0300

    Add files via upload

---
+++ src/main/java/br/edu/ifsp/sd_catalogo/SdCatalogoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.sd_catalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdCatalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdCatalogoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsp/sd_catalogo/SdCatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.sd_catalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdCatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
