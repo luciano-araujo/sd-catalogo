5135d50963415ce17ce8af7dc2c32612afaf8846
Luciano Araujo <araujo.luciano1302@gmail.com>
2026-04-19 19:13:50 -0300

    Add global exception handler to manage runtime exceptions

---
+++ src/main/java/br/edu/ifsp/sd_catalogo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package br.edu.ifsp.sd_catalogo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Object> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(Map.of("erro", ex.getMessage()));
+    }
+}
